05231073c32779547f1baa090563ff5468ae7acb
Nikita <nvs38@tpu.ru>
2021-11-11 17:11:47 +0700

    started with go

--- api-builder/main.go
+++ api-builder/main.go
@@ -14,14 +14,25 @@ func main() {
 		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
 	})
+	// Query string parameters are parsed using the existing underlying request object.
+	// The request responds to a url matching:  /welcome?firstname=Jane&lastname=Doe
+	router.GET("/welcome", func(c *gin.Context) {
+		firstname := c.DefaultQuery("firstname", "New Guest")
+		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
 
+		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
 	// However, this one will match /user/john/ and also /user/john/send
 	// If no other routers match /user/john, it will redirect to /user/john/
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
+	router.POST("/form_post", func(c *gin.Context) {
+		message := c.PostForm("message")
+		nick := c.DefaultPostForm("nick", "anonymous")
+
+		c.JSON(200, gin.H{
+			"status":  "posted",
+			"message": message,
+			"nick":    nick,
+		})
 	})
 
 	// For each matched request Context will hold the route definition
